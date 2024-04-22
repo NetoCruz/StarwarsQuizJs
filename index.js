0b2d5a2d80409a4cf3a70f2a9e2c373a3066ab11
Neto Cruz <galaxy.net0710@gmail.com>
2024-04-22 15:19:53 -0600

    quiz 1.0

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ data=[
         option1:"Yoda",
         option2:"Qui-gon-jin",
         option3:"Dooku",
+        correct:2
     },
     {
         id:2,
@@ -13,6 +14,7 @@ data=[
         option1:"Yoda",
         option2:"Palpatine",
         option3:"Darth Maul",
+        correct:2
     }
 ]
 let startBtn = document.getElementById('start'); 
@@ -25,7 +27,7 @@ let count = 0;
 startBtn.addEventListener('click', function () { 
     timer = true; 
     stopWatch(); 
-   myquiz()
+   myquiz(0)
 }); 
   
 stopBtn.addEventListener('click', function () { 
@@ -40,38 +42,52 @@ resetBtn.addEventListener('click', function () {
     document.getElementById('count').innerHTML = "00"; 
 }); 
 
-function myquiz(){
-     
+function myquiz(nextq){
+     let numQuestion=nextq;
         const question = document.createElement("h4");
-question.innerHTML =data[0].quiz;
+        question.setAttribute("id", "titleQ")
+question.innerHTML =data[numQuestion].quiz;
 document.body.appendChild(question)
 
 const opt1 = document.createElement("button");
 opt1.setAttribute("id", "op1")
-opt1.innerHTML =data[0].option1;
+opt1.innerHTML =data[numQuestion].option1;
 document.body.appendChild(opt1)
 
 const opt2 = document.createElement("button");
 opt2.setAttribute("id","op2")
-opt2.innerHTML =data[0].option2;
+opt2.innerHTML =data[numQuestion].option2;
 document.body.appendChild(opt2)
 
 const opt3 = document.createElement("button");
 opt3.setAttribute("id","op3")
-opt3.innerHTML =data[0].option3;
+opt3.innerHTML =data[numQuestion].option3;
 document.body.appendChild(opt3)
         // document.getElementById('question').innerHTML = item.quiz;
         // document.getElementById('op1').innerHTML = item.option1;
-        // document.getElementById('op2').innerHTML = item.option2;
-        // document.getElementById('op3').innerHTML = item.option3;
+        
 
     let option3 = document.getElementById('op3')
 option3.addEventListener('click',function(){
     document.getElementById('scoreNum').innerHTML = "1";
-})    
+    eraseQuestion()
+    myquiz(1)
+})  
    
 }
+function eraseQuestion(){
+    const question = document.getElementById("titleQ");
+    question.remove()
+    const opt1 = document.getElementById("op1");
+    opt1.remove()
 
+const opt2 = document.getElementById("op2");
+    opt2.remove()
+
+const opt3 = document.getElementById("op3");
+  opt3.remove()
+}
+  
   
   
 function stopWatch() { 
